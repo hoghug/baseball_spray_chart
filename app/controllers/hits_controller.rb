70993b237e3bbc01c77bde31c3cb77d499ee183b
chris lind <clind714@gmail.com>
2014-05-08 14:12:40 -0700

    doubleclick to add, click on existing hit to delete

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -7,6 +7,17 @@ class HitsController < ApplicationController
       format.html { redirect_to player_path(@player) }
       format.js
     end
+  end
+
+  def destroy
+    @player = Player.find(params[:player_id])
+    @hit = Hit.find(params[:id])
+    @hit.destroy
+
+    respond_to do |format|
+      format.html { redirect_to player_path(@player) }
+      format.js
+    end
 
   end
 
